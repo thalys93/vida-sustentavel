305aaf6d45d400a855a3cca9cf84e307bb386795
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-16 14:06:22 -0300

    Added Styling in Contact Page and About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,83 +1,100 @@
-import { Col, Container, Figure, Row } from 'react-bootstrap'
+import { Accordion, Col, Container, Figure, Row } from 'react-bootstrap'
 import NavegationComponent from '../components/NavegationComponent'
 
 function About() {
-  const primaryColor = "primaryColor"  
   return (
     <>
-    <NavegationComponent customClass={primaryColor}/>
-    <Container fluid>
-      <Row>
-        <Col sm>
-          <section>
-            <article>
-              <div className='lightPopup Bebas'>
-                <h3> Quem Somos </h3>
-              </div>
-              <div className='lightPopup BigShoulders'>
-                <p>
-                  Bem-vindo ao <span>Vida Sustentável</span>, onde a paixão pelo planeta e o compromisso com um futuro mais <span>verde</span> se encontram. Nossa história é uma jornada de amor pela natureza, um desejo de mudança e o compromisso de inspirar outros a se juntarem a nós nesse movimento <span>vital.</span>
+      <NavegationComponent />
+      <Container fluid className='spacingText'>
+        <Row className='mb-5'>
+          <Col sm>
+            <section>
+              <article>
+                <div className='lightPopup Bebas text-center'>
+                  <h3 className='p-2'> Quem Somos </h3>
+                </div>
+                <div className='lightPopup BigShoulders mt-2'>
+                  <p className='p-2'>
+                    Bem-vindo ao <span className='greenSecondary fw-bold'>Vida Sustentável</span>, onde a paixão pelo planeta e o compromisso com um futuro mais <span className='greenSecondary fw-bold'>verde</span> se encontram. Nossa história é uma jornada de amor pela natureza, um desejo de mudança e o compromisso de inspirar outros a se juntarem a nós nesse movimento <span className='greenSecondary fw-bold'>vital.</span>
                   </p>
-              </div>
-            </article>
-          </section>
-        </Col>
+                </div>
+              </article>
+            </section>
+          </Col>
 
-        <Col sm>
-          <section>
-            <article>
-              <div>
-                <h3>Nossa Missão</h3>
-              </div>
-              <div>
-                <p>
-                No <span>Vida Sustentável</span>, nossa <span>missão é clara e inspiradora:</span> capacitar indivíduos e comunidades a adotar práticas de <span>vida sustentável,</span> transformando o nosso mundo em um lugar mais saudável, equilibrado e vibrante. Acreditamos que todos têm um papel a desempenhar na construção de um futuro mais sustentável e que juntos podemos fazer a <span>diferença.</span>
-                </p>
-              </div>
-            </article>
-          </section>
-        </Col>          
-      </Row>
-      <Row>
-      <Col sm>
-          <section>
-            <article>
-              <div>
-                <h3> O Que Nos Move </h3>
-              </div>
-              <div>
-                <ul>
-                  <li>
-                    <span>Apaixonados pela Natureza:</span>
-                  </li>
-                  <li>
-                    <span>Conscientização dos Desafios Ambientais:</span>
-                  </li>
-                  <li>
-                    <span>Compromisso com as Gerações Futuras:</span>
-                  </li>
-                </ul>
-              </div>
-            </article>
-          </section>
-        </Col>
+          <Col sm>
+            <section>
+              <article>
+                <div className='lightPopup Bebas text-center'>
+                  <h3 className='p-2'>Nossa Missão</h3>
+                </div>
+                <div className='lightPopup BigShoulders mt-2'>
+                  <p className='p-2'>
+                    No <span className='greenSecondary fw-bold'>Vida Sustentável</span>, nossa <span className='greenSecondary fw-bold'>missão é clara e inspiradora:</span> capacitar indivíduos e comunidades a adotar práticas de <span className='greenSecondary fw-bold'>vida sustentável,</span> transformando o nosso mundo em um lugar mais saudável, equilibrado e vibrante. Acreditamos que todos têm um papel a desempenhar na construção de um futuro mais sustentável e que juntos podemos fazer a <span className='greenSecondary fw-bold'>diferença.</span>
+                  </p>
+                </div>
+              </article>
+            </section>
+          </Col>
+        </Row>
 
-        <Col sm>
-          <section>
-            <article>
-              <div>
-                <Figure>
-                  <Figure.Image
-                  src="img/aboutUsDay.png"
-                  />
-                <Figure.Caption>El Cap, Yosemite National Park, United States</Figure.Caption>
-                </Figure>
-              </div>
-            </article>
-          </section>
-        </Col>         
-      </Row>
-    </Container>
+        <Row>
+          <Col sm className='mb-4'>
+            <section>
+              <article>
+                <div className='lightPopup Bebas text-center'>
+                  <h3 className='p-2'> O Que Nos Move </h3>
+                </div>
+                <div className='lightPopup BigShoulders mt-2'>
+                  <Accordion flush alwaysOpen>
+                    <Accordion.Item eventKey='0'>
+                      <Accordion.Header>
+                        <span className='greenSecondary'>Apaixonados pela Natureza:</span>
+                      </Accordion.Header>
+                      <Accordion.Body>
+                        <p> Expressar um profundo amor e apreço pela beleza e diversidade da natureza que nos motiva a protegê-la. </p>
+                      </Accordion.Body>
+                    </Accordion.Item>
+
+                    <Accordion.Item eventKey='1'>
+                      <Accordion.Header>
+                        <span className='greenSecondary'>Conscientização dos Desafios Ambientais:</span>
+                      </Accordion.Header>
+                      <Accordion.Body>
+                        <p> Reconhecimento dos desafios ambientais urgentes, como as mudanças climáticas, poluição e perda de biodiversidade. </p>
+                      </Accordion.Body>
+                    </Accordion.Item>
+
+                    <Accordion.Item eventKey='2'>
+                      <Accordion.Header>
+                        <span className='greenSecondary'>Compromisso com as Gerações Futuras:</span>
+                      </Accordion.Header>
+                      <Accordion.Body>
+                        <p> compromisso de criar um mundo sustentável para as gerações futuras, onde elas possam desfrutar de um planeta saudável e equilibrado. </p>
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </div>
+              </article>
+            </section>
+          </Col>
+
+          <Col sm>
+            <section>
+              <article>
+                <div>
+                  <Figure>
+                    <Figure.Image
+                      src="img/aboutUsDay.png"
+                    />
+                    <Figure.Caption>El Cap, Yosemite National Park, United States</Figure.Caption>
+                  </Figure>
+                </div>
+              </article>
+            </section>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,75 +1,83 @@
-import { Col, Container, Figure, Row } from "react-bootstrap" 
-import {BsLinkedin, BsInstagram, BsGithub, BsGlobe} from "react-icons/bs"
+import { Col, Container, Figure, Row, Stack } from "react-bootstrap"
+import { BsLinkedin, BsInstagram, BsGithub, BsGlobe } from "react-icons/bs"
 import NavegationComponent from "../components/NavegationComponent"
 
 
 function Contact() {
 
-  const primaryColor = "primaryColor"
 
   return (
     <>
-    <NavegationComponent customClass={primaryColor}/>
-    <Container fluid>
-      <Row>
-        <Col sm>
-          <section>
-            <article>
-              <div>
-                <h3> Aviso Importante: </h3>
-              </div>
-              <p>
-              Esta Solução é uma Jornada de Estudo e Criatividade
-O Vida Sustentável é mais do que apenas um site. É uma jornada de descoberta, aprendizado e inovação em design de interface e experiência do usuário (UI/UX). Fomos inspirados a criar esta plataforma como um projeto de paixão para explorar como a   sustentabilidade pode ser comunicada de maneira eficaz.
-              </p>
-            </article>
-          </section>
-        </Col>
+      <NavegationComponent />
+      <Container fluid className="spacingText">
+        <Row>
+          <Col sm>
+            <section>
+              <article>
+                <div className='lightPopup Bebas text-center'>
+                  <h3 className="p-2"> Aviso Importante: </h3>
+                </div>
+                <p className="p-2 lightPopup BigShoulders">
+                  Esta Solução é uma Jornada de Estudo e Criatividade
+                  O Vida Sustentável é mais do que apenas um site. É uma jornada de descoberta, aprendizado e inovação em design de interface e experiência do usuário (UI/UX). Fomos inspirados a criar esta plataforma como um projeto de paixão para explorar como a   sustentabilidade pode ser comunicada de maneira eficaz.
+                </p>
+              </article>
+            </section>
+          </Col>
 
-        <Col sm>
-          <section>
-            <article>
-              <div>
-                <h3> Contato </h3>
-              </div>
-              <div>
-                {ContactIcons()}
-              </div>
-              <p>
-              Se você está buscando soluções de design de 
-              <span>UI/UX</span> que reflitam a essência da sustentabilidade e se adaptem à sua marca e visão de negócios, você está no lugar certo. Embora este projeto tenha sido desenvolvido para <span>fins de estudo,</span> estamos ansiosos para colaborar em projetos sob medida para <span>empresas</span> que compartilham nossa paixão pela sustentabilidade.
-              </p>
-            </article>
-          </section>
-        </Col>
-      </Row>
-    </Container>
+          <Col sm>
+            <section>
+              <article>
+                <div className='lightPopup Bebas text-center'>
+                  <h3> Contato </h3>
+                </div>
+                <div className="lightPopup m-3 text-center">
+                  {ContactIcons()}
+                </div>
+                <p className="p-2 lightPopup BigShoulders">
+                  Se você está buscando soluções de design de
+                  <span className="greenSecondary fw-bold"> UI/UX</span> que reflitam a essência da sustentabilidade e se adaptem à sua marca e visão de negócios, você está no lugar certo. Embora este projeto tenha sido desenvolvido para <span className="greenSecondary fw-bold">fins de estudo,</span> estamos ansiosos para colaborar em projetos sob medida para <span className="greenSecondary fw-bold">empresas</span> que compartilham nossa paixão pela sustentabilidade.
+                </p>
+              </article>
+            </section>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 
   function ContactIcons() {
-    return <Row>
-      <Col sm>
-        <Figure>
-          <BsLinkedin/>
+    return <Stack direction="horizontal" gap={4} className="p-3 justify-content-center">
+      <Figure className="figureHover">
+        <a href="https://www.linkedin.com/in/luis-rodrigues202/" target="_blank" rel="noopener noreferrer" className="text-decoration-none primaryColor">
+          <BsLinkedin className="iconSize" />
           <Figure.Caption> Linkedin </Figure.Caption>
-        </Figure>
-        <Figure>
-          <BsInstagram/> 
+        </a>
+      </Figure>
+
+
+      <Figure className="figureHover">
+        <a href="https://www.instagram.com/luiss_xavierr/" target="_blank"  rel="noopener noreferrer"className="text-decoration-none primaryColor">
+          <BsInstagram className="iconSize" />
           <Figure.Caption> Instagram </Figure.Caption>
-        </Figure>
-      </Col>
-      <Col sm>
-        <Figure>
-          <BsGithub/>
+        </a>
+      </Figure>
+
+
+      <Figure className="figureHover">
+        <a href="https://github.com/thalys93" target="_blank" rel="noopener noreferrer" className="text-decoration-none primaryColor">
+          <BsGithub className="iconSize" />
           <Figure.Caption> Github </Figure.Caption>
-        </Figure>
-        <Figure>
-          <BsGlobe/>
+        </a>
+      </Figure>
+
+      <Figure className="figureHover">
+        <a href="https://portifolio-luis-thalys.web.app/" target="_blank" rel="noopener noreferrer"className="text-decoration-none primaryColor">
+          <BsGlobe className="iconSize" />
           <Figure.Caption> Portifólio </Figure.Caption>
-        </Figure>
-      </Col>
-    </Row>    
+        </a>
+      </Figure>
+    </Stack>
   }
 }
 
