23b06019c74d7df0d87382fdb1cd672d60d280e6
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-16 16:00:56 -0300

    fixed routes.jsx path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './assets/css/global.css'
 import { DarkModeProvider } from './utils/DarkModeContext.jsx'
-import Routes from './utils/Routes'
+import Routes from './utils/routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <DarkModeProvider>
