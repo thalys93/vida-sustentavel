175bfc1a4722f82a10d3f10d16537eef5afef16e
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-16 14:06:02 -0300

    Fixed HomePage Responsivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,49 +6,70 @@ function Home() {
   const [windowSize, setWindowSize] = useState(window.innerWidth)
 
   useEffect(() => {
-  const handleRezise = () => setWindowSize(window.innerWidth)  
-  window.addEventListener('resize', handleRezise);
-  return () => {
-    window.removeEventListener('resize', handleRezise);
-  };
-}, []);  
+    const handleRezise = () => setWindowSize(window.innerWidth)
+    window.addEventListener('resize', handleRezise);
+    return () => {
+      window.removeEventListener('resize', handleRezise);
+    };
+  }, []);
 
-const primaryColor = "whiteColor"
-const margin = "customMargin"
+  const primaryColor = "whiteColor"  
 
 
-  return (       
+  return (
     <>
-    {windowSize >= 579 ? (    
-      <NavegationComponent customClass={primaryColor} margin={margin}/>          
-    ) : (      
-      <NavegationComponent/>
-    )}
-      <Container fluid>
+      {windowSize >= 579 ? (
+        <NavegationComponent customClass={primaryColor}/>
+      ) : (
+        <NavegationComponent />
+      )}
+      <Container fluid className="mt-lg-5">
         <Row>
-          <Col sm={windowSize >= 576 ? true : false}>       
-            {windowSize >= 579 ? (                         
-            HomeDesktop()
+          <Col sm={windowSize >= 576 ? true : false}>
+            {windowSize >= 579 ? (
+              HomeDesktop()
             ) : (
-            HomeMobile()
-            )}       
-          </Col> 
-          {windowSize >= 579 ? (         
-          <Col sm>   
-            <Image src="img/homeVetor.png" className="imageHome img-fluid "  width={732}/>                  
+              HomeMobile()
+            )}
           </Col>
-          ) : null}
         </Row>
-      </Container>       
-      </>   
+      </Container>
+    </>
   )
 
   function HomeDesktop() {
-    return <section className="m-3 ms-5 ">
+    return <section className="m-lg-5 ms-lg-5 m-md-2 ms-md-4">
       <article>
+        <Container fluid>
+          <Row>
+            <Col md>
+              <div className="flex flex-column pb-3">
+                <h1 className="Bebas primaryColor spacingTitle"> Faça a <br /> diferença <span className="greenPrimary"> Hoje </span></h1>
+                <h5 className="BigShoulders tertiaryColor spacingSubtitle fw-bolder "> Juntos Podemos Construir <br />um futuro mais <span className="greenPrimary"> Verde </span></h5>
+              </div>
+              <div>
+                <a href="/about">
+                  <button className="GreenBtn whiteColor Bebas w-50"><h4>Saiba Mais</h4> </button>
+                </a>
+              </div>
+            </Col>
+              <Col md>
+                <Container fluid>
+                  <Image src="img/homeVetor.png" className="imageHome" width={windowSize >= 1025.233 ? null : windowSize >= 768 ? "57%" : "0%"}/>
+                </Container>
+              </Col>            
+          </Row>
+        </Container>
+      </article>
+    </section>
+  }
+
+  function HomeMobile() {
+    return <section>
+      <article className="mobileText">
         <div className="flex flex-column pb-3">
-          <h1 className="Bebas primaryColor"> Faça a <br /> diferença <span className="greenPrimary"> Hoje </span></h1>
-          <h5 className="BigShoulders tertiaryColor"> Juntos Podemos Construir <br />um futuro mais <span className="greenPrimary"> Verde </span></h5>
+          <h1 className="Bebas primaryColor spacingTitle"> Faça a <br /> diferença <span className="greenPrimary"> Hoje </span></h1>
+          <h2 className="BigShoulders tertiaryColor spacingSubtitle"> Juntos Podemos Construir um <br /> futuro mais <span className="greenPrimary"> Verde </span></h2>
         </div>
         <div>
           <a href="/about">
@@ -57,22 +78,6 @@ const margin = "customMargin"
         </div>
       </article>
     </section>;
-  }
-
-  function HomeMobile() {
-    return <section>
-    <article>
-      <div className="flex flex-column pb-3">
-        <h1 className="Bebas primaryColor"> Faça a <br /> diferença <span className="greenPrimary"> Hoje </span></h1>
-        <h5 className="BigShoulders tertiaryColor"> Juntos Podemos Construir <br />um futuro mais <span className="greenPrimary"> Verde </span></h5>
-      </div>
-      <div>
-        <a href="/about">
-          <button className="GreenBtn whiteColor Bebas"><h4>Saiba Mais</h4> </button>
-        </a>
-      </div>
-    </article>
-  </section>;
   }
 }
 
