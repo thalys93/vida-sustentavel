5961bda90e2314f762c0cb1bcc746b3da236ad39
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-16 23:30:54 -0300

    added acessibility things to darkmode component

--- src/components/DarkModeComponent.jsx
+++ src/components/DarkModeComponent.jsx
@@ -1,14 +1,23 @@
 import { useContext } from 'react'
 import { BsMoonStars, BsSun } from 'react-icons/bs'
 import { DarkModeContext } from '../utils/DarkModeContext.jsx'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 function DarkModeComponent() {
-    const {isDarkMode , toggleDarkMode } = useContext(DarkModeContext)  
+  const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext)
+
+  const renderTooltip = (props) => (
+    <Tooltip {...props}>
+      Mudar para o modo {isDarkMode ? 'claro' : 'escuro'}?
+    </Tooltip>
+  )
 
   return (
-    <div className={isDarkMode? 'position-relative p-2 m-3 end-0 bg-dark rounded-pill whiteColor' : 'position-relative p-2 m-3 end-0 bg-white rounded-pill primaryColor'} onClick={toggleDarkMode}>
-    {!isDarkMode ? <BsMoonStars className="DarkIconSize"/> : <BsSun className="DarkIconSize"/>}
-  </div>
+    <OverlayTrigger overlay={renderTooltip} placement='bottom'>
+      <div className={isDarkMode ? 'position-relative p-2 m-3 end-0 bg-dark rounded-pill whiteColor' : 'position-relative p-2 m-3 end-0 bg-white rounded-pill primaryColor'} onClick={toggleDarkMode}>
+        {!isDarkMode ? <BsMoonStars className="DarkIconSize" /> : <BsSun className="DarkIconSize" />}
+      </div>
+    </OverlayTrigger>
   )
 }
 
